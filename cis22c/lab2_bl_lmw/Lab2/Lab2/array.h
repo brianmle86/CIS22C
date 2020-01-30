86a0d6e245f24803a48db696235196ad6f67c50e
brianmle86 <41857702+brianmle86@users.noreply.github.com>
2020-01-29 17:13:12 -0800

    added lab2 submission

---
+++ cis22c/lab2_bl_lmw/Lab2/Lab2/array.h
@@ -0,0 +1,54 @@
+#pragma once
+#ifndef ARRAY_H
+#define ARRAY_H
+#include <string>
+#include <iostream>
+
+using namespace std;
+/*
+Template array class.
+This allows us to create an array of a generic type, T.
+
+*/
+template <class T>
+class Array {
+private:
+	T* dataArray;
+	int size;
+public:
+	Array();
+	Array(int givenSize);
+	virtual ~Array();
+
+	//operator overloads
+	friend ostream& operator<<(ostream& output, Array<T>* obj) {
+		for (int i = 0; i < size; i++) {
+			output << obj[i];
+		}
+		return output;
+
+	}
+	T& operator[](int i) {
+		return dataArray[i];
+	}
+
+};
+
+
+template <class T>
+Array<T>::Array() {
+	size = 0;
+}
+
+template <class T>
+Array<T>::Array(int givenSize) {
+	dataArray = new T[givenSize];
+	size = givenSize;
+}
+
+template <class T>
+Array<T>::~Array() {
+	delete[] dataArray;
+}
+
+#endif
---
+++ cis22c/lab2_bl_lmw/Lab2/Lab2/main.cpp
@@ -0,0 +1,68 @@
+/*
+Brian Le, Luke Marshall
+De Anza College
+CIS22C - Lab 2
+
+*/
+
+#include <iostream>
+#include <string>
+#include "array.h"
+#include "sort.h"
+#include "currency.h"
+using namespace std;
+
+const int MAX_SORT_SIZE = 16;
+
+int main() {
+	int numElements;
+	string type;
+
+	cout << "Please enter the number of elements you wish to sort: ";
+	cin >> numElements;
+
+	while (numElements > MAX_SORT_SIZE || numElements <= 1) {
+		cout << "Invalid! Try again: ";
+		cin >> numElements;
+	}
+
+	cout << "Please enter the desired element type(int, string, Dollar): ";
+	cin >> type;
+	while (type != "int" && type != "string" && type != "Dollar") {
+		cout << "Invalid type! Try again: ";
+		cin >> type;
+	}
+
+	if (type == "int") {
+		Array<int> dataArray(numElements);
+
+		for (int i = 0; i < numElements; i++) {
+			cout << "Please enter integer element #" << i + 1 << ": ";
+			cin >> dataArray[i];
+		}
+		Sort<int>::RecurSelectionSort(dataArray, numElements);
+
+	}
+
+	else if (type == "string") {
+		Array<string> dataArray(numElements);
+		for (int i = 0; i < numElements; i++) {
+			cout << "Please enter string element #" << i + 1 << ": ";
+			cin >> dataArray[i];
+		}
+		Sort<string>::RecurSelectionSort(dataArray, numElements);
+	}
+
+	else if (type == "Dollar") {
+		Array<Dollar> dataArray(numElements);
+		for (int i = 0; i < numElements; i++) {
+			cout << "Please enter Dollar element #" << i + 1 << " in the form wholeParts, fractionParts: ";
+			cin >> dataArray[i];
+		}
+		Sort<Dollar>::RecurSelectionSort(dataArray, numElements);
+	}
+
+
+	system("pause");
+	return 0;
+}
