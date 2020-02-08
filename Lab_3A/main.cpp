df5919dbd3944bb740ecd517dd0d9f780558720b
Lucas Marshall <lukeawild@outlook.com>
2020-02-07 20:45:07 -0800

    worked on creating lists in main and tested sort.h

--- Lab_3A/main.cpp
+++ Lab_3A/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include "node.h"
+#include "list.h"
+#include "currency.h"
+#include "sort.h"
+
+
 using namespace std;
 int main() {
+	int data;
+	string dataString;
+
+	List <int> intList;
+	List <string> stringList;
+	List <Rupee> rupeeList;
+
+
+
+	for (int i = 0; i < 12; i++) {
+		cout << "Please enter integer element #" << i + 1 << ": ";
+		cin >> data;
+		intList.pushNode(data);
+	}
+	Sort<int>::RecurSelectionSort(intList, 12);
+
+
+	for (int i = 0; i < 20; i++) {
+		cout << "Please enter string element #" << i + 1 << ": ";
+		cin >> dataString;
+		stringList.pushNode(dataString);
+	}
+	Sort<string>::RecurSelectionSort(stringList, 12);
+
+	for (int i = 0; i < 11; i++) {
+		cout << "Please enter Dollar element #" << i + 1 << " in the form wholeParts, fractionParts: ";
+		cin >> data;
+		rupeeList.pushNode(data);
+	}
+	
+
 	cout << "this is lab 3a main\n";
 	return 0;
 }
---
+++ Lab_3A/sort.h
@@ -0,0 +1,104 @@
+#pragma once
+#ifndef SORT_H
+#define SORT_H
+#include "list.h"
+#include <iostream>
+#include <fstream>
+ofstream outFile;
+
+/*
+Sort template class.
+This needs to be a template class because our sort function
+needs to sort ints, strings, and Dollars.
+We are using static functions because we are not creating objects of Sort.
+
+*/
+template <class T>
+class Sort {
+private:
+    T* data;
+    int size;
+
+public:
+    Sort(List<T> lst, int s);
+    virtual ~Sort();
+
+    /*
+    This function sorts recursively with a recursive selection sort(descending order).
+    While doing so, it displays and writes to a file "out.txt" the array each
+    time it is called. Function static because we'll use it with no object.
+    */
+    static void RecurSelectionSort(List<T>& toSort, int size, int x = 0);
+
+    //simple swap function
+    static void swapVal(T& a, T& b);
+};
+
+
+template <class T>
+Sort<T>::Sort(List<T> lst, int s) {
+    data = lst;
+    size = s;
+
+}
+
+template <class T>
+Sort<T>::~Sort() {
+    delete[] data;
+}
+
+template <class T>
+void Sort<T>::RecurSelectionSort(List<T>& toSort, int size, int x) {
+    if (size - x == size) {//initial call, when x is 0 (default argument)
+        outFile.open("out.txt");
+        outFile << "Brian Le, Luke Marshall\nCIS22C - Lab 3A\n\n";
+        cout << "\ninput array: ";
+        outFile << "input array: ";
+        for (int i = 0; i < size; i++) {
+            cout << toSort[i] << " ";
+            outFile << toSort[i] << " ";
+        }
+        cout << endl;
+        outFile << endl;
+    }
+    if (size - x == 1) {
+        outFile.close();
+        return;
+    }
+    else {
+        T max = toSort[x]; //assume first value is largest
+        int maxIndex = x; //therefore, set that index as maxIndex
+
+        for (int i = x; i < size; ++i) {
+            if (toSort[i] > max) { //found a value that's larger than the current max
+                max = toSort[i];
+                maxIndex = i;
+            }
+        }
+
+        swapVal(toSort[x], toSort[maxIndex]);
+
+        for (int i = 0; i < size; i++) { //display to console, write to file
+            cout << toSort[i] << " ";
+            outFile << toSort[i] << " ";
+
+        }
+        cout << endl;
+        outFile << endl;
+        RecurSelectionSort(toSort, size, x + 1);
+
+
+    }
+}
+
+template <class T>
+void Sort<T>::swapVal(T& a, T& b) {
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+
+
+#endif
