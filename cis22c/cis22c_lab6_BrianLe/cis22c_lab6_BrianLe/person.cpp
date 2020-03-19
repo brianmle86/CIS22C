9642a18c5f6bd0b67cc45f4d22a320555fe7caa2
brianmle86 <41857702+brianmle86@users.noreply.github.com>
2020-03-18 17:51:29 -0700

    Revert "Revert "added lab 6 submission""
    This reverts commit d7bda7b7c2ee0a273bee1beb5903d6730900161b.

---
+++ cis22c/cis22c_lab6_BrianLe/cis22c_lab6_BrianLe/person.cpp
@@ -0,0 +1,75 @@
+#include "person.h"
+
+Person::Person() {}
+
+Person::Person(std::string n, std::string day) {
+	name = n;
+	bday = day;
+}
+
+std::string Person::getName() {
+	return name;
+}
+
+std::string Person::getBday() {
+	return bday;
+}
+
+void Person::setName(std::string n) {
+	name = n;
+}
+
+void Person::setBday(std::string day) {
+	bday = day;
+}
+
+void Person::setPkey(int key) {
+	if (key == 0)
+		pkey = &name;
+
+	else if (key == 1)
+		pkey = &bday;
+}
+
+/*
+From canvas:
+ For the print method, output the object on a single 80-char line formatted to print
+ name left aligned in 60-chars followed by birthday right aligned in 20-chars
+*/
+void Person::displayPerson() {
+	std::cout << std::left << std::setw(60) << name << std::right << std::setw(20) << bday;
+}
+
+std::ostream& operator<<(std::ostream& output, Person& obj) {
+	output << std::left << std::setw(60) << obj.getName() << std::right << std::setw(20) << obj.getBday();
+	return output;
+}
+
+std::istream& operator>>(std::istream& input, Person& obj) {
+	input >> obj.name >> obj.bday;
+	return input;
+}
+
+/*
+From canvas:
+compare data based on dereference of the pkey attribute
+*/
+bool operator< (Person& obj1, Person& obj2) {
+	return *(obj1.pkey) < *(obj2.pkey);
+}
+
+bool operator> (Person& obj1, Person& obj2) {
+	return *(obj1.pkey) > * (obj2.pkey);
+}
+
+bool operator== (Person& obj1, Person& obj2) {
+	return *(obj1.pkey) == *(obj2.pkey);
+}
+
+bool operator<= (Person& obj1, Person& obj2) {
+	return *(obj1.pkey) <= *(obj2.pkey);
+}
+
+bool operator>= (Person& obj1, Person& obj2) {
+	return *(obj1.pkey) <= *(obj2.pkey);
+}
